341d75515bebd1533819e952025b6f86b85d5947
Karen Martinez <karenmartinezriv527@gmail.com>
2025-05-10 18:13:42 -0600

    Se hizo la guia de vistas y rutas de larravel, se crearon parra categorias, productos y clientes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,42 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home',['nombre'=>'Karen', 'Apellido'=>'Martinez']);
+});
+
+//Productos
+// Ruta para mostrar la vista show.blade.php
+Route::get('/products/show', function () {
+    return view('products/show', ['producto'=>'Camisa Sport']);
+});
+// Ruta para mostrar la vista create.blade.php
+Route::get('/products/create', function () {
+    return view('products/create');
+});
+// Ruta para mostrar la vista update.blade.php
+Route::get('/products/edit', function () {
+    return view('products/update');
+});
+
+//Clientes
+
+Route::get('/clients/show', function(){
+    return view('clients/show');
+});
+Route::get('/clients/create', function(){
+    return view('clients/create');
+});
+Route::get('/clients/edit', function(){
+    return view('clients/update');
+});
+
+//Categorias
+Route::get('categories/show', function(){
+    return view('categories/show');
+});
+Route::get('categories/create', function(){
+    return view('categories/create');
+});
+Route::get('categories/edit', function(){
+    return view('categories/update');
 });
